c6227d8a9ab26c35e77d05725efdcb8c1c951d96
刘镕硕 <99574908+lrsgzs@users.noreply.github.com>
2022-12-21 10:06:20 +0800

    给yls这个仓库用的

---
+++ yls.js
@@ -0,0 +1,31 @@
+peoples = {
+    "编程部 - Scratch部": {
+        "巩佳宁": "https://code.xueersi.com/space/19162616",
+        "周小荻": "https://code.xueersi.com/space/19776819",
+    },
+    "编程部 - Python部": {
+        "周小荻": "https://code.xueersi.com/space/19776819",
+        "杨欣儒": "https://code.xueersi.com/space/16074710",
+    },
+    "编程部 - C++部": {
+        "空": "#"
+    },
+    "编程部 - 动画部": {
+        "盛翊展": "https://code.xueersi.com/space/60701655",
+    },
+    "编程部 - 跑酷部": {
+        "盛翊展": "https://code.xueersi.com/space/60701655",
+    },
+    "编程部 - 改编部": {
+        "空": "#"
+    },
+    "外交部": {
+        "刘某杰": "https://code.xueersi.com/space/78145585",
+    },
+    "宣传部": {
+        "周利贞": "https://code.xueersi.com/space/60177039",
+    },
+    "成员": {
+        "杨云骢": "https://code.xueersi.com/space/12502420",
+    }
+}
